d113030f34307d5646a0ad7c7ec90edb8a21b48a
SaiProton <shayanr1001@gmail.com>
2023-06-04 21:11:57 -0400

    refactor: simplified/generalized compare instruction's indirect testing methods

--- src/cpu/instructions/set/compare/test_templates/indirect.rs
+++ src/cpu/instructions/set/compare/test_templates/indirect.rs
@@ -1,92 +1,68 @@
 use super::*;
 
 pub fn x_compare(opcode: u8, target: &RegisterAlias) {
+    let load_data_and_run = |cpu: &mut Cpu, register_val, memory_val| {
+        cpu.load_program(&[opcode, 0x40]);
+
+        cpu.registers.set_register(&RegisterAlias::X, 0x02);
+        cpu.registers.set_register(target, register_val);
+
+        cpu.memory.write(0x0042, 0x69);
+        cpu.memory.write(0x0069, memory_val);
+
+        cpu.run();
+    };
+
     let mut cpu = Cpu::new();
 
     // Equality case.
-    cpu.load_program(&[opcode, 0x40]);
-
-    cpu.registers.set_register(&RegisterAlias::X, 0x02);
-    cpu.registers.set_register(target, 0x50);
-
-    cpu.memory.write(0x0042, 0x69);
-    cpu.memory.write(0x0069, 0x50);
-
-    cpu.run();
+    load_data_and_run(&mut cpu, 0x50, 0x50);
 
     assert!(cpu.registers.status.get_flag(StatusFlagAlias::C));
     assert!(cpu.registers.status.get_flag(StatusFlagAlias::Z));
 
     // Greater case.
-    cpu.load_program(&[opcode, 0x40]);
-
-    cpu.registers.set_register(&RegisterAlias::X, 0x02);
-    cpu.registers.set_register(target, 0x50);
-
-    cpu.memory.write(0x0042, 0x69);
-    cpu.memory.write(0x0069, 0x4F);
-
-    cpu.run();
+    load_data_and_run(&mut cpu, 0x50, 0x4F);
 
     assert!(cpu.registers.status.get_flag(StatusFlagAlias::C));
     assert!(!cpu.registers.status.get_flag(StatusFlagAlias::Z));
 
     // Less case.
-    cpu.load_program(&[opcode, 0x40]);
-
-    cpu.registers.set_register(&RegisterAlias::X, 0x02);
-    cpu.registers.set_register(target, 0x50);
-
-    cpu.memory.write(0x0042, 0x69);
-    cpu.memory.write(0x0069, 0x51);
-
-    cpu.run();
+    load_data_and_run(&mut cpu, 0x50, 0x51);
 
     assert!(!cpu.registers.status.get_flag(StatusFlagAlias::C));
     assert!(!cpu.registers.status.get_flag(StatusFlagAlias::Z));
 }
 
 pub fn y_compare(opcode: u8, target: &RegisterAlias) {
+    let load_data_and_run = |cpu: &mut Cpu, register_val, memory_val| {
+        cpu.load_program(&[opcode, 0x42]);
+
+        cpu.registers.set_register(&RegisterAlias::Y, 0x09);
+        cpu.registers.set_register(target, register_val);
+
+        cpu.memory.write(0x0042, 0x60);
+        cpu.memory.write(0x0069, memory_val);
+
+        cpu.run();
+    };
+
     let mut cpu = Cpu::new();
 
     // Equality case.
-    cpu.load_program(&[opcode, 0x42]);
-
-    cpu.registers.set_register(&RegisterAlias::Y, 0x09);
-    cpu.registers.set_register(target, 0x50);
-
-    cpu.memory.write(0x0042, 0x60);
-    cpu.memory.write(0x0069, 0x50);
-
-    cpu.run();
+    load_data_and_run(&mut cpu, 0x50, 0x50);
 
     assert!(cpu.registers.status.get_flag(StatusFlagAlias::C));
     assert!(cpu.registers.status.get_flag(StatusFlagAlias::Z));
 
     // Greater case.
-    cpu.load_program(&[opcode, 0x42]);
-
-    cpu.registers.set_register(&RegisterAlias::Y, 0x09);
-    cpu.registers.set_register(target, 0x50);
-
-    cpu.memory.write(0x0042, 0x60);
-    cpu.memory.write(0x0069, 0x4F);
-
-    cpu.run();
+    load_data_and_run(&mut cpu, 0x50, 0x4F);
 
     assert!(cpu.registers.status.get_flag(StatusFlagAlias::C));
     assert!(!cpu.registers.status.get_flag(StatusFlagAlias::Z));
 
     // Less case.
-    cpu.load_program(&[opcode, 0x42]);
-
-    cpu.registers.set_register(&RegisterAlias::Y, 0x09);
-    cpu.registers.set_register(target, 0x50);
-
-    cpu.memory.write(0x0042, 0x60);
-    cpu.memory.write(0x0069, 0x51);
-
-    cpu.run();
+    load_data_and_run(&mut cpu, 0x50, 0x51);
 
     assert!(!cpu.registers.status.get_flag(StatusFlagAlias::C));
     assert!(!cpu.registers.status.get_flag(StatusFlagAlias::Z));
