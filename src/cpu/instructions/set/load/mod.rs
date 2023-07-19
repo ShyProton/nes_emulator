a3f7cf3a4d01540b2b81c2cfe45dd50e25ef22d0
ShyProton <shayanr1001@gmail.com>
2023-07-18 22:26:46 -0400

    refactor: load tests overhauled, now tests all cases iteratively

--- src/cpu/instructions/set/load/mod.rs
+++ src/cpu/instructions/set/load/mod.rs
@@ -1,14 +1,10 @@
 use super::{aliases::RegisterAlias, AddressingMode, Cpu};
 
 #[cfg(test)]
-use super::aliases::StatusFlagAlias;
-
-mod lda;
-mod ldx;
-mod ldy;
+use super::{aliases::StatusFlagAlias, test_prep};
 
 #[cfg(test)]
-mod test_templates;
+mod tests;
 
 impl Cpu {
     /// LD_ - Load a Register.
@@ -20,5 +16,26 @@ impl Cpu {
 
         self.registers.set_register(target, result);
         self.update_zero_and_negative_flags(result);
+    }
+
+    /// LDA - Load Accumulator.
+    /// Loads a byte of memory into the accumulator setting the zero and negative flags as
+    /// appropriate.
+    pub fn lda(&mut self, addr_mode: &AddressingMode) {
+        self.load(addr_mode, &RegisterAlias::A);
+    }
+
+    /// LDX - Load X Register.
+    /// Loads a byte of memory into the X register setting the zero and negative flags as
+    /// appropriate.
+    pub fn ldx(&mut self, addr_mode: &AddressingMode) {
+        self.load(addr_mode, &RegisterAlias::X);
+    }
+
+    /// LDY - Load Y Register.
+    /// Loads a byte of memory into the Y register setting the zero and negative flags as
+    /// appropriate.
+    pub fn ldy(&mut self, addr_mode: &AddressingMode) {
+        self.load(addr_mode, &RegisterAlias::Y);
     }
 }
---
+++ src/cpu/instructions/set/load/tests.rs
@@ -0,0 +1,85 @@
+use super::{
+    test_prep,
+    AddressingMode::{
+        self, Absolute, AbsoluteX, AbsoluteY, Immediate, IndirectX, IndirectY, ZeroPage, ZeroPageX,
+        ZeroPageY,
+    },
+    Cpu, RegisterAlias, StatusFlagAlias,
+};
+use std::{collections::HashMap, iter::zip};
+
+type OpCodeModes = (Vec<u8>, Vec<AddressingMode>);
+
+fn base_load(opcode: u8, addr_mode: &AddressingMode, target: &RegisterAlias) {
+    let mut cpu = Cpu::new();
+
+    test_prep::prepare(&mut cpu, opcode, addr_mode, 0x55);
+    cpu.run();
+
+    assert_eq!(cpu.registers.get_register(target), 0x55);
+    assert!(!cpu.registers.status.get_flag(StatusFlagAlias::Z));
+    assert!(!cpu.registers.status.get_flag(StatusFlagAlias::N));
+}
+
+fn base_flag_check(opcode: u8, addr_mode: &AddressingMode) {
+    let mut cpu = Cpu::new();
+
+    test_prep::prepare(&mut cpu, opcode, addr_mode, 0x00);
+    cpu.run();
+
+    assert!(cpu.registers.status.get_flag(StatusFlagAlias::Z));
+    assert!(!cpu.registers.status.get_flag(StatusFlagAlias::N));
+
+    test_prep::prepare(&mut cpu, opcode, addr_mode, 0b1000_0000);
+    cpu.run();
+
+    assert!(!cpu.registers.status.get_flag(StatusFlagAlias::Z));
+    assert!(cpu.registers.status.get_flag(StatusFlagAlias::N));
+}
+
+fn get_instruction_map() -> HashMap<RegisterAlias, OpCodeModes> {
+    HashMap::from([
+        (
+            RegisterAlias::A,
+            (
+                vec![0xA9, 0xA5, 0xB5, 0xAD, 0xBD, 0xB9, 0xA1, 0xB1],
+                vec![
+                    Immediate, ZeroPage, ZeroPageX, Absolute, AbsoluteX, AbsoluteY, IndirectX,
+                    IndirectY,
+                ],
+            ),
+        ),
+        (
+            RegisterAlias::X,
+            (
+                vec![0xA2, 0xA6, 0xB6, 0xAE, 0xBE],
+                vec![Immediate, ZeroPage, ZeroPageY, Absolute, AbsoluteY],
+            ),
+        ),
+        (
+            RegisterAlias::Y,
+            (
+                vec![0xA0, 0xA4, 0xB4, 0xAC, 0xBC],
+                vec![Immediate, ZeroPage, ZeroPageX, Absolute, AbsoluteX],
+            ),
+        ),
+    ])
+}
+
+#[test]
+fn load() {
+    for (register, (opcodes, addr_modes)) in get_instruction_map() {
+        for (op, mode) in zip(opcodes, addr_modes) {
+            base_load(op, &mode, &register);
+        }
+    }
+}
+
+#[test]
+fn flag_check() {
+    for (_, (opcodes, addr_modes)) in get_instruction_map() {
+        for (op, mode) in zip(opcodes, addr_modes) {
+            base_flag_check(op, &mode);
+        }
+    }
+}
